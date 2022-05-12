90e206df5b6f03f43749ac40e21fbdaff68e8f60
KLQUINTERO <quintero.karen@correounivalle.edu.co>
2022-05-11 20:09:26 -0500

    Te amo muchisisismo

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("Te amoo <3");
 console.log("TE AMO, GRACIAS POR ESTAR AQUI CONMIGO");
 
 console.log("No tienes nada que agradecer, tu sabes que te amo con todo mi ser");
+console.log("Eres mi todo");
